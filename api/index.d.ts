df55e2c086b8d5321919ea16a0a49e9afcf6bf3b
Syed Muzamil <smmhd121@gmail.com>
2023-05-31 16:39:43 +0530

    Tests

--- api/index.d.ts
+++ api/index.d.ts
@@ -1,10 +1,10 @@
 import { Request } from 'express'
-import { User } from './src/models/user'
+import { UserWithId } from './src/models/user'
 
 declare global {
   namespace Express {
     export interface Request {
-      user: Omit<User, 'password' | 'salt'> | null
+      user: Omit<UserWithId, 'password' | 'salt'> | null
     }
   }
 }
--- api/test/auth.test.ts
+++ api/test/auth.test.ts
@@ -25,4 +25,22 @@ describe('POST /api/v1/auth/signup', () => {
         expect(response.body).toHaveProperty('tokens')
       })
   })
+
+  it('responds with error', async () => {
+    request(app)
+      .get('/api/v1/auth/signup')
+      .set('Accept', 'application/json')
+      .send({
+        name: 'Syed Muzamil',
+        email: 'smmhd121@gmail.com',
+        role: 'caregiver',
+        password: 'testpassword',
+        stage: 'pre-conception',
+      })
+      .expect('Content-Type', /json/)
+      .expect(422)
+      .then((response) => {
+        expect(response.body).toHaveProperty('message')
+      })
+  })
 })
