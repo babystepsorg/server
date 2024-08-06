b0efb3c2e43b6076b7a9b713fcb29ebcf71714e9
Syed Muzamil <smmhd121@gmail.com>
2024-08-06 22:39:21 +0530

    Issues fixed

--- api/src/api/tracking/tracking.route.ts
+++ api/src/api/tracking/tracking.route.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import * as TrackingHandler from './tracking.handler'
+import { validateAuthentication } from '../../middlewares'
 
 const router = Router()
 
 router.post(
     '/screen-time',
+    validateAuthentication,
     TrackingHandler.storeTrackingData
 )
 
